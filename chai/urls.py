92c1b3161341badeed784b4b6f6c6e6ad3d8acc2
Kunj <kunjjadav1806@gmail.com>
2025-05-28 14:57:15 +0530

    added chai folder along with order in chai

--- chai/urls.py
+++ chai/urls.py
@@ -1,8 +1,7 @@
-# chai/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chai, name='chai'),  
-    path('order/',views.order, name='order')
+    path('',views.chai, name='chai'),
+    path('order/',views.order,name="order")
 ]
